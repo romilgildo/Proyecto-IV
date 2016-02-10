fe74c59efdb8120ea291f67ec213bc1e976c0d3e
Romilgildo <rubenmartin1991@gmail.com>
2016-02-10 20:43:45 +0100

    ultimos ajustes docker

--- variables_entorno.sh
+++ variables_entorno.sh
@@ -2,13 +2,13 @@
 
 archivo=./despliegueMySQL/.env
 
-echo 'VM_USER=""' > $archivo
-echo 'VM_PASS=""' >> $archivo
-echo 'AZURE_PEM=""' >> $archivo
-echo 'SUBSCRIPTION_ID=""' >> $archivo
-echo 'VM_MYSQL_NAME=""' >> $archivo
-echo 'VM_DOCKER_NAME=""' >> $archivo
-echo 'VM_APP_NAME=""' >> $archivo
+echo 'VM_USER="pluco"' > $archivo
+echo 'VM_PASS="PLUCOdb2#"' >> $archivo
+echo 'AZURE_PEM="~/azure.pem"' >> $archivo
+echo 'SUBSCRIPTION_ID="a5c45913-5302-4f3e-9ac2-77b0c0883196"' >> $archivo
+echo 'VM_MYSQL_NAME="pluco-bd"' >> $archivo
+echo 'VM_DOCKER_NAME="pluco-docker"' >> $archivo
+echo 'VM_APP_NAME="pluco-app"' >> $archivo
 
 cp $archivo ./despliegueAzure
 cp $archivo ./despliegueDocker
